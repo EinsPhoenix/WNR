bb58fe8582c79521d3061c74e2eb4558711b23e2
NichtJFDochIchKennedy <jan.thomas0506@gmail.com>
2025-05-28 07:21:58 +0200

    Calibration working / start on dll implementation

--- Robot/source/dobot_control.py
+++ Robot/source/dobot_control.py
@@ -1,25 +1,45 @@
 from json import dumps, loads
-from math import sqrt, atan2, cos, sin
-from pydobot import Dobot
 from socket import socket, AF_INET, SOCK_STREAM
+from sys import path
 from time import sleep
 
+path.insert(1,'./DLL')
 
-class Robot:
-    def __init__(self, speed: int = 500) -> None:
+import DobotDllType as dobot_type
+
+
+CON_STR = {
+    dobot_type.DobotConnect.DobotConnect_NoError:  "DobotConnect_NoError",
+    dobot_type.DobotConnect.DobotConnect_NotFound: "DobotConnect_NotFound",
+    dobot_type.DobotConnect.DobotConnect_Occupied: "DobotConnect_Occupied"
+}
+
+
+class DoBotControl:
+    def __init__(self, homeX: float = 300, homeY: float = 0, homeZ: float = 0, speed: int = 500) -> None:
         """
-        Initialize the Robot class.
+        Initialize the DoBotControl class.
 
         Args:
+            homeX (float, optional): The X coordinate of the home position. Default is 300.
+            homeY (float, optional): The Y coordinate of the home position. Default is 0.
+            homeZ (float, optional): The Z coordinate of the home position. Default is 0.
             speed (int, optional): The speed of the robot. Default is 500.
         """
-        self.device = Dobot(port="COM5", verbose=False)
+        self.suction: bool = False
+        self.api = dobot_type.load()
+        self.homeX: float = homeX
+        self.homeY: float = homeY
+        self.homeZ: float = homeZ
+        self.connected: bool = False
+        self.dobot_connect()
         if speed > 2000:
             speed = 2000
         elif speed < 100:
             speed = 100
         self.speed: int = speed
-        self.device.speed(speed, speed)
+        # FIXME: Implement speed setting in DoBotArm class
+        # self.robot.speed(speed, speed)
         # FIXME: Check if pos is good
         self.green_storage: list[tuple[float, float], int] = [(0, 300), 0]
         self.blue_storage: list[tuple[float, float], int] = [(20, 280), 0]
@@ -27,14 +47,42 @@ class Robot:
         self.yellow_storage: list[tuple[float, float], int] = [(20, -280), 0]
 
     def __del__(self) -> None:
-        """Destructor for the Robot class. Disconnects the Dobot device."""
-        self.disconnect()
+        """Destructor for the DoBotControl class. Disconnects the Dobot device."""
+        self.dobot_disconnect()
 
-    def disconnect(self) -> None:
-        """Disconnect the Dobot device."""
-        self.move_to_position(300, 0, 0)
-        self.device.close()
+    def dobot_connect(self, velocity: int = 100, acceleration: int = 100, joint_velocity: int = 200, joint_acceleration: int = 200) -> None:
+        """
+        Attempts to connect to the Dobot device.
 
+        Args:
+            velocity (int, optional): The velocity for the robot. Default is 100.
+            acceleration (int, optional): The acceleration for the robot. Default is 100.
+            joint_velocity (int, optional): The joint velocity for the robot. Default is 200.
+            joint_acceleration (int, optional): The joint acceleration for the robot. Default is 200.
+        """
+        if not self.connected:
+            state = dobot_type.ConnectDobot(self.api, "", 115200)[0]
+            if state == dobot_type.DobotConnect.DobotConnect_NoError:
+                dobot_type.SetQueuedCmdClear(self.api)
+                dobot_type.SetHOMEParams(self.api, self.homeX, self.homeY, self.homeZ, 0, isQueued=1)
+                dobot_type.SetPTPJointParams(self.api, joint_velocity, joint_acceleration, joint_velocity, joint_acceleration,
+                    joint_velocity, joint_acceleration, joint_velocity, joint_acceleration, isQueued=1)
+                dobot_type.SetPTPCommonParams(self.api, velocity, acceleration, isQueued=1)
+                dobot_type.SetHOMECmd(self.api, temp=0, isQueued=1)
+                self.connected = True
+            else:
+                raise ConnectionError(f"Unable to connect. Connect status: {CON_STR[state]}")
+
+    def dobot_disconnect(self) -> None:
+        self.move_home()
+        dobot_type.DisconnectDobot(self.api)
+        self.connected = False
+
+    def move_home(self) -> None:
+        """Move the robot to the home position."""
+        lastIndex = dobot_type.SetPTPCmd(self.api, dobot_type.PTPMode.PTPMOVLXYZMode, self.homeX, self.homeY, self.homeZ, 0)[0]
+        self.command_delay(lastIndex)
+        
     def send_message(self, message_to_send: str, host: str = "localhost", port: int = 65432) -> None:
         """
         Send a command to the rasberry pi, which controls the camera.
@@ -57,6 +105,22 @@ class Robot:
         except Exception as e:
             print(f"An error occurred: {e}")
 
+    def move_to_position(self, x: float, y: float, z: float | None = None) -> None:
+        """
+        Move the robot to a specified position.
+
+        Args:
+            x (float): The X coordinate of the target position.
+            y (float): The Y coordinate of the target position.
+            z (float | None, optional): The Z coordinate of the target position.
+            r (float | None, optional): The rotation angle of the head.
+        """
+        if z is None:
+            # FIXME: Hier muss ich noch die aktuelle Z-Position auslesen
+            pass
+        lastIndex = dobot_type.SetPTPCmd(self.api, dobot_type.PTPMode.PTPMOVLXYZMode, x, y, z, 0)[0]
+        self.command_delay(lastIndex)
+
     def calibrate_camera(self) -> None:
         """Calibrate the camera by moving the robot to specific position."""
         calibrate_positions: list[tuple[int, int]] = [(0, -300), (200, -200), (300, 0), (200, 200), (0, 300)]
@@ -65,82 +129,12 @@ class Robot:
             while self.send_message({"type": "calibrate", "payload": {"number": i, "robot_pos": {"x": pos[0], "y": pos[1]}}})["status"] == "error":
                 sleep(0.5)
 
-    def get_current_robot_pos(self) -> tuple[float, float, float, float]:
-        """
-        Get the current x, y, z and r position of the robot.
-
-        Returns:
-            tuple[float, float, float, float]: The current x, y, z, and r positions of the robot.
-        """
-        (x, y, z, r, j1, j2, j3, j4) = self.device.pose()
-        return (x, y, z, r)
-
-    def move_to_position(self, target_x: float, target_y: float, target_z: float | None = None, target_r: float | None = None) -> None:
-        """
-        Move the robot to the specified position.
-
-        Args:
-            x (float): The target x position.
-            y (float): The target y position.
-            z (float | None, optional): The target z position. Default is None.
-            r (float | None, optional): The target r position. Default is None.
-        """
-        current_x, current_y, current_z, current_r = self.get_current_robot_pos()
-        if target_z is None:
-            target_z = current_z
-        if target_r is None:
-            target_r = current_r
-        measure_amount = int(self.speed / 10) + 1
-        current_radius: float = sqrt(pow(current_x, 2) + pow(current_y, 2))
-        target_radius: float = sqrt(pow(target_x, 2) + pow(target_y, 2))
-        target_z += (target_radius - current_radius) / 4
-        current_angle: float = atan2(current_y, current_x)
-        target_angle: float = atan2(target_y, target_x)
-        angle_diff: float = target_angle - current_angle
-        radii: list[float] = [current_radius + i * (target_radius - current_radius) / (measure_amount - 1) for i in range(measure_amount)]
-        print(f"Moving from ({current_x}, {current_y}, {current_z}) to ({target_x}, {target_y}, {target_z})")
-        for i in range(1, measure_amount):
-            angle: float = current_angle + angle_diff / (measure_amount - 1) * i
-            x: float = radii[i] * cos(angle)
-            y: float = radii[i] * sin(angle)
-            z: float = current_z + (target_z - current_z) / (measure_amount - 1) * i
-            r: float = current_r + (target_r - current_r) / (measure_amount - 1) * i
-            print(f"Moving to: ({x:.2f}, {y:.2f}, {z:.2f})")
-            self.device.move_to(x, y, z, r, wait=True)
-            sleep(0.1)
-
-    def move_block_to_storage(self, block_x: float, block_y: float, color: str) -> None:
-        """
-        Move the robot to the storage position based on the color.
-
-        Args:
-            block_x (float): The x position of the block.
-            block_y (float): The y position of the block.
-            color (str): The color of the block.
-        """
-        # FIXME: Check if hight is good
-        self.move_to_position(block_x, block_y, 0)
-        self.device.suck(True)
-        target_storage = getattr(self, f"{color}_storage")
-        storage_x, storage_y = target_storage[0]
-        storage_level: int = target_storage[1]
-        storage_z: float = storage_level * 25
-        storage_radius: float = sqrt(pow(storage_x, 2) + pow(storage_y, 2))
-        target_radius: float = storage_radius - 50
-        scale: float = target_radius / storage_radius
-        target_x: float = storage_x * scale
-        target_y: float = storage_y * scale
-        self.move_to_position(target_x, target_y, storage_z)
-        self.move_to_position(storage_x, storage_y, storage_z)
-        self.device.suck(False)
-        getattr(self, f"{color}_storage")[1] += 1
-        self.move_to_position(target_x, target_y, storage_z)
-
-
-def main() -> None:
-    robot = Robot(1000)
-    robot.move_to_position(0, -300)
-
+    def command_delay(self, lastIndex):
+        # FIXME: implement function that can execute commands instantly
+        dobot_type.SetQueuedCmdStartExec(self.api)
+        while lastIndex > dobot_type.GetQueuedCmdCurrentIndex(self.api)[0]:
+            dobot_type.dSleep(200)
+        dobot_type.SetQueuedCmdStopExec(self.api)
 
 if __name__ == "__main__":
-    main()
+    dobot_control = DoBotControl()
